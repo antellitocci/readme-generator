5faef7021d4a8d83bf72271c89442a13d30c1536
raddymaster <antellitocci@gmail.com>
2021-06-11 00:40:39 -0400

    inital question prompts, markdown generation, and README creation

--- src/markdown-template.js
+++ src/markdown-template.js
@@ -1,7 +1,66 @@
-const generateMarkdown = (projectData) => {
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+
+};
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  //https://choosealicense.com/appendix/
+};
+
+//function to get license badges and add them 
+
+const generateMarkdown = projectData => {
     //create variables from projectData
-    //const {} = projectData;
-    return `# ${projectData.title}
-  
+    console.log(projectData);
+    const { license, title, language, description, installation, usage, contributing, tests, github, email, questions } = projectData;
+    return `# ${title}
+// license bade
+
+## Desciption\n
+${description}
+
+## Built With
+${language}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributing
+${contributing}
+
+## License
+${license}
+
+## Tests
+${tests}
+
+## Questions
+${questions}
+${email}
+
+### &copy; //function to return current year  ${github} 
   `;
   }
+
+  module.exports = generateMarkdown;
--- utils/generate-readme.js
+++ utils/generate-readme.js
@@ -1,16 +1,20 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const generateReadMe = data => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('dist/README.md', data, err => {
+      //if error send to catch() method
+      if(err){
+        reject(err);
+        return;
+      }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  //https://choosealicense.com/appendix/
-}
-
+      resolve({
+        ok: true,
+        message: 'README generated!'
+      });
+    });
+  });
+};
 
 module.exports = generateReadMe;
