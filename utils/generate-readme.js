86622ebdad5978af20b11e4d8dfc21683d82edd6
raddymaster <antellitocci@gmail.com>
2021-06-13 19:20:57 -0400

    added error catch if dist directory does not exist

--- utils/generate-readme.js
+++ utils/generate-readme.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
+const dir = 'dist';
 
 const generateReadMe = data => {
   return new Promise((resolve, reject) => {
+    //make sure the dist directory exists. Create it if not.
+    if(!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
     fs.writeFile('dist/README.md', data, err => {
       //if error send to catch() method
       if(err){
