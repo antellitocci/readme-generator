3e78a30bd8033245e416827c676b8132ead18e65
raddymaster <antellitocci@gmail.com>
2021-06-13 15:03:57 -0400

    beginning of license badge function

---
+++ dist/README.md
@@ -0,0 +1,40 @@
+# test
+// license bade
+
+        
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+      
+
+## Description
+
+test
+
+## Built With
+HTML
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+ete
+
+## Usage
+tse
+
+## Contributing
+tetse
+
+## Tests
+ets
+
+## Questions
+es
+set
+[et](https://github.com/et)
+  
--- src/markdown-template.js
+++ src/markdown-template.js
@@ -1,31 +1,32 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
+//require node fetch
+const fetch = require('node-fetch');
 
-};
+//get current year
+const thisYear = new Date().getFullYear();
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
+//license information array
+const licenseArr = [
+  {name: "Apache 2.0", badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)", content:""},
+  {name: "MIT", badge: "", content:""},
+  {name: "GPL 3.0", badge: "", content:""},
+  {name: "BSD 3.0", badge: "", content:""},
+  {name: "LGPL 2.1", badge: "", content:""},
+  {name: "None", badge: "", content:""}
+];
 
-};
+//language information array
+const languageArr = [
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  //https://choosealicense.com/appendix/
-};
-
-//function to get license badges and add them 
-
+];
 const generateMarkdown = projectData => {
     //create variables from projectData
     console.log(projectData);
     const { license, title, language, description, installation, usage, contributing, tests, github, email, questions } = projectData;
     return `# ${title}
 // license bade
+${renderLicenseBadge(license)}
 
-## Desciption\n
+## Description\n
 ${description}
 
 ## Built With
@@ -49,18 +50,35 @@ ${usage}
 ## Contributing
 ${contributing}
 
-## License
-${license}
-
 ## Tests
 ${tests}
 
 ## Questions
 ${questions}
 ${email}
-
-### &copy; //function to return current year  ${github} 
+[${github}](https://github.com/${github})
   `;
-  }
+};
 
-  module.exports = generateMarkdown;
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  for (var i=0; i < licenseArr.length; i++){
+    if(license = licenseArr[i].name && license != "None"){
+      return `
+        \n${licenseArr[i].badge}
+      `;
+    }
+  }
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+
+};
+
+//function to get license badges and add them 
+
+
+module.exports = generateMarkdown;
